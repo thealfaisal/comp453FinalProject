d0508ddb5c1d1730cbf185de433866de1e23ae02
Samgbr <smlgbr@gmail.com>
2018-12-04 16:22:14 -0600

    DDL Created and ER Updated

--- flaskDemo/__init__.py
+++ flaskDemo/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://cfn:naiman@localhost/company'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://User1:dbUser1@localhost/company'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
